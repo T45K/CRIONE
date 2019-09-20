2f693042af9ffb657f9f89094c59fcaf84a4fa19
T45K <tasktas9@gmail.com>
2019-09-21 00:26:00 +0900

    change argument: String -> Repository

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMiner.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMiner.kt
@@ -2,17 +2,12 @@ package jp.ac.osaka_u.ist.sdl.crione.repositoryMining
 
 import org.eclipse.jgit.lib.Repository
 import org.refactoringminer.api.GitHistoryRefactoringMiner
-import org.refactoringminer.api.GitService
 import org.refactoringminer.api.Refactoring
 import org.refactoringminer.api.RefactoringHandler
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl
-import org.refactoringminer.util.GitServiceImpl
 
-fun mining(repositoryPath: String): List<String> {
-    val gitService: GitService = GitServiceImpl()
-    val repository: Repository = gitService.cloneIfNotExists(repositoryPath, "https://github.com/danilofes/refactoring-toy-example.git")
+fun mining(repository: Repository): List<String> {
     val miner: GitHistoryRefactoringMiner = GitHistoryRefactoringMinerImpl()
-
     val extractMethodCommitHashes: MutableList<String> = mutableListOf()
     miner.detectAll(repository, "master", object : RefactoringHandler() {
         override fun handle(commitId: String?, refactorings: List<Refactoring>?) {
--- src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMinerKtTest.kt
+++ src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMinerKtTest.kt
@@ -1,12 +1,22 @@
 package jp.ac.osaka_u.ist.sdl.crione.repositoryMining
 
+import org.eclipse.jgit.lib.Repository
 import org.junit.Test
+import org.refactoringminer.api.GitService
+import org.refactoringminer.util.GitServiceImpl
 import kotlin.test.assertEquals
 
 class RefactoringMinerKtTest {
+    companion object {
+        private const val projectDir: String = "sample/refactoring-toy-example"
+        private const val cloneURL: String = "https://github.com/danilofes/refactoring-toy-example.git"
+    }
+
     @Test
     fun testMining() {
-        val hashes: List<String> = mining("sample/refactoring-toy-example")
+        val gitService: GitService = GitServiceImpl()
+        val repository: Repository = gitService.cloneIfNotExists(projectDir, cloneURL)
+        val hashes: List<String> = mining(repository)
         assertEquals(2, hashes.size)
         assertEquals("f35b2c8eb8c320f173237e44d04eefb4634649a2", hashes[0])
         assertEquals("40950c317bd52ea5ce4cf0d19707fe426b66649c", hashes[1])
