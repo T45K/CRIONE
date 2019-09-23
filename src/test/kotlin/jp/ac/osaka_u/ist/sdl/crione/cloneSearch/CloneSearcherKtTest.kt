ea6b96505737fcd183dde4be18d74e368f8eae20
T45K <tasktas9@gmail.com>
2019-09-23 14:51:04 +0900

    fix args

--- src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/cloneSearch/CloneSearcherKtTest.kt
+++ src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/cloneSearch/CloneSearcherKtTest.kt
@@ -11,13 +11,13 @@ class CloneSearcherKtTest {
     @Test
     fun testSearch() {
         val query = "int a = 0; char b = '0'; double c = 0d; float d = 0f; long e = 0l; String f = \"0\";"
-        val fileList: List<Pair<Path, String>> = listOf(getSourceCode(Paths.get("sample/cloneSearch/File1.java")), getSourceCode(Paths.get("sample/cloneSearch/File2.java")), getSourceCode(Paths.get("sample/cloneSearch/File3.java")))
+        val fileList: List<Pair<String, String>> = listOf(getSourceCode(Paths.get("sample/cloneSearch/File1.java")), getSourceCode(Paths.get("sample/cloneSearch/File2.java")), getSourceCode(Paths.get("sample/cloneSearch/File3.java")))
 
         val clones: List<Clone> = search(query, fileList)
         assertEquals(3, clones.size)
 
         val (filePath0, beginIndex0, endIndex0, code0) = clones[0]
-        assertEquals(Paths.get("sample/cloneSearch/File1.java"), filePath0)
+        assertEquals("sample/cloneSearch/File1.java", filePath0)
         assertEquals(85, beginIndex0)
         assertEquals(206, endIndex0)
         assertEquals("""int a = 0;
@@ -28,7 +28,7 @@ class CloneSearcherKtTest {
         String f = "0";""", code0)
 
         val (filePath1, beginIndex1, endIndex1, code1) = clones[1]
-        assertEquals(Paths.get("sample/cloneSearch/File1.java"), filePath1)
+        assertEquals("sample/cloneSearch/File1.java", filePath1)
         assertEquals(264, beginIndex1)
         assertEquals(380, endIndex1)
         assertEquals("""int g = 1;
@@ -39,7 +39,7 @@ class CloneSearcherKtTest {
         String l = "1";""", code1)
 
         val (filePath2, beginIndex2, endIndex2, code2) = clones[2]
-        assertEquals(Paths.get("sample/cloneSearch/File2.java"), filePath2)
+        assertEquals("sample/cloneSearch/File2.java", filePath2)
         assertEquals(84, beginIndex2)
         assertEquals(205, endIndex2)
         assertEquals("""int g = 1;
@@ -50,7 +50,7 @@ class CloneSearcherKtTest {
         String l = "1";""", code2)
     }
 
-    private fun getSourceCode(filePath: Path): Pair<Path, String> {
-        return filePath to String(Files.readAllBytes(filePath))
+    private fun getSourceCode(filePath: Path): Pair<String, String> {
+        return filePath.toString() to String(Files.readAllBytes(filePath))
     }
 }
