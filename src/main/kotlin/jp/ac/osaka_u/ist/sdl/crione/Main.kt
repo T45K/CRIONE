90b7249f13bfdde313136e5e8fb74de6873c2240
T45K <tasktas9@gmail.com>
2019-09-23 14:33:19 +0900

    impl main

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/Main.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/Main.kt
@@ -1,12 +1,25 @@
 package jp.ac.osaka_u.ist.sdl.crione
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello world."
-        }
-}
+import jp.ac.osaka_u.ist.sdl.crione.cloneSearch.Clone
+import jp.ac.osaka_u.ist.sdl.crione.cloneSearch.search
+import jp.ac.osaka_u.ist.sdl.crione.repositoryMining.MyRepository
+import jp.ac.osaka_u.ist.sdl.crione.repositoryMining.mining
+import org.eclipse.jgit.lib.Repository
+import org.refactoringminer.util.GitServiceImpl
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    println(App().greeting)
+    val (projectDir, cloneURL, srcDir) = args
+    val repository: Repository = GitServiceImpl().cloneIfNotExists(projectDir, cloneURL)
+    val miningResult: List<String> = mining(repository)
+
+    val myRepository = MyRepository(repository)
+    for (commitId: String in miningResult) {
+        myRepository.checkout(commitId)
+        val sourceCodes: List<Pair<String, String>> = myRepository.getSourceCodes(Paths.get(projectDir, srcDir))
+        val deletedDiffs: List<String> = myRepository.getDeletedDiffs()
+
+        val clones: List<List<Clone>> = deletedDiffs.map { query -> search(query, sourceCodes) }
+        println(clones)
+    }
 }
