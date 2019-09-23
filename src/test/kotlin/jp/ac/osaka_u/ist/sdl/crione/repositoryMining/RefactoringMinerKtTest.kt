79361e531f0cedf6ccb0b2d56200ae97893bd912
T45K <tasktas9@gmail.com>
2019-09-23 14:56:03 +0900

    add singleton

--- src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMinerKtTest.kt
+++ src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMinerKtTest.kt
@@ -1,21 +1,14 @@
 package jp.ac.osaka_u.ist.sdl.crione.repositoryMining
 
+import jp.ac.osaka_u.ist.sdl.crione.testEntity.TestInstance
 import org.eclipse.jgit.lib.Repository
 import org.junit.Test
-import org.refactoringminer.api.GitService
-import org.refactoringminer.util.GitServiceImpl
 import kotlin.test.assertEquals
 
 class RefactoringMinerKtTest {
-    companion object {
-        private const val projectDir: String = "sample/refactoring-toy-example"
-        private const val cloneURL: String = "https://github.com/danilofes/refactoring-toy-example.git"
-    }
-
     @Test
     fun testMining() {
-        val gitService: GitService = GitServiceImpl()
-        val repository: Repository = gitService.cloneIfNotExists(projectDir, cloneURL)
+        val repository: Repository = TestInstance.repositoryInstance
         val hashes: List<String> = mining(repository)
         assertEquals(2, hashes.size)
         assertEquals("f35b2c8eb8c320f173237e44d04eefb4634649a2", hashes[0])
