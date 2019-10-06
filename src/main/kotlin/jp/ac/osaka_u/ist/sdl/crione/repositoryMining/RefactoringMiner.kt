ba2a532ee44c1de5ebf6db2a2c6e5b6ab0d801b0
tsk-nkgw <tasktas9@gmail.com>
2019-10-06 23:09:50 +0900

    excellent way

--- src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMiner.kt
+++ src/main/kotlin/jp/ac/osaka_u/ist/sdl/crione/repositoryMining/RefactoringMiner.kt
@@ -1,26 +1,34 @@
 package jp.ac.osaka_u.ist.sdl.crione.repositoryMining
 
+import gr.uom.java.xmi.diff.ExtractOperationRefactoring
 import org.eclipse.jgit.lib.Repository
 import org.refactoringminer.api.GitHistoryRefactoringMiner
 import org.refactoringminer.api.Refactoring
 import org.refactoringminer.api.RefactoringHandler
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl
 
-fun mining(repository: Repository, trackingBranch: String): List<String> {
+fun mining(repository: Repository, trackingBranch: String): List<Pair<String, String>> {
     val miner: GitHistoryRefactoringMiner = GitHistoryRefactoringMinerImpl()
-    val extractMethodCommitHashes: MutableList<String> = mutableListOf()
+    val extractedCodeAndCommitHashes: MutableList<Pair<String, String>> = mutableListOf()
     miner.detectAll(repository, trackingBranch, object : RefactoringHandler() {
         override fun handle(commitId: String?, refactorings: List<Refactoring>?) {
-            val containsExtractMethod = refactorings?.any(::isExtractMethodRefactoring) ?: false
-            if (containsExtractMethod) {
-                extractMethodCommitHashes.add(commitId!!)
-            }
+            extractedCodeAndCommitHashes.addAll(refactorings!!.filter(::isExtractMethodRefactoring)
+                    .map { flatStatements(it as ExtractOperationRefactoring) to commitId!! }
+                    .toList())
         }
     })
 
-    return extractMethodCommitHashes
+    return extractedCodeAndCommitHashes
 }
 
 private fun isExtractMethodRefactoring(refactoring: Refactoring): Boolean {
     return refactoring.name == "Extract Method"
+}
+
+private fun flatStatements(refactoring: ExtractOperationRefactoring): String {
+    return refactoring.extractedOperation
+            .body
+            .compositeStatement
+            .statements
+            .joinToString("") { it.toString() }
 }
