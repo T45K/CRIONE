0140903121c481dcb838f64f199416479306a963
T45K <tasktas9@gmail.com>
2019-11-29 01:02:27 +0900

    fix test

--- src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/ConfigTest.kt
+++ src/test/kotlin/jp/ac/osaka_u/ist/sdl/crione/ConfigTest.kt
@@ -8,7 +8,7 @@ class ConfigTest {
 
     @Test
     fun configTest1() {
-        val (mode: Mode, projectDir, cloneURL, srcDir, trackingBranch) = buildFromArgs(listOf("-m", "s", "-p", "sample/refactoring-toy-example", "-c", "https://github.com/danilofes/refactoring-toy-example.git", "-s", "src"))
+        val (mode: Mode, projectDir, cloneURL, srcDir, trackingBranch) = buildFromArgs(listOf("-m", "s", "-p", "sample/refactoring-toy-example", "-c", "https://github.com/danilofes/refactoring-toy-example.git", "-s", "src"))!!
         assertThat(mode == Mode.SEARCH)
         assertEquals("sample/refactoring-toy-example", projectDir)
         assertEquals("https://github.com/danilofes/refactoring-toy-example.git", cloneURL)
@@ -18,7 +18,7 @@ class ConfigTest {
 
     @Test
     fun configTest2() {
-        val (mode: Mode, projectDir, cloneURL, srcDir, trackingBranch) = buildFromArgs(listOf("-m", "m", "-p", "sample/refactoring-toy-example", "-c", "https://github.com/danilofes/refactoring-toy-example.git", "-s", "src", "-t", "trunk"))
+        val (mode: Mode, projectDir, cloneURL, srcDir, trackingBranch) = buildFromArgs(listOf("-m", "m", "-p", "sample/refactoring-toy-example", "-c", "https://github.com/danilofes/refactoring-toy-example.git", "-s", "src", "-t", "trunk"))!!
         assertThat(mode == Mode.MINING)
         assertEquals("sample/refactoring-toy-example", projectDir)
         assertEquals("https://github.com/danilofes/refactoring-toy-example.git", cloneURL)
@@ -31,7 +31,7 @@ class ConfigTest {
         buildFromArgs(listOf("-m", "hoge", "-p", "sample/refactoring-toy-example", "-s", "src", "-s", "source"))
     }
 
-    @Test(expected = RuntimeException::class)
+    @Test
     fun configTest4() {
         buildFromArgs(listOf())
     }
